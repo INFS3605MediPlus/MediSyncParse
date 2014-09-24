36a0829e498b47705e3defedaf467a7a214ee333
mootz99 <muthu.udai99@gmail.com>
2014-09-24 22:51:13 +1000

    Updating create patient
    Updating create patient

--- js/my_js.js
+++ js/my_js.js
@@ -1,17 +1,40 @@
 
-
-
 function createNewPatientIntoParse() {
     var newPatientfname = document.getElementById("firstNameOfPatient").value;
     var newPatientlname = document.getElementById("lastNameOfPatient").value;
-        parseCreatePatient(newPatientfname, newPatientlname);
+    var newPatientemail = document.getElementById("emailOfPatient").value;
+    var newPatientaddress = document.getElementById("addressOfPatient").value;
+    var newPatientDOB = document.getElementById("DOBOfPatient").value;
+    var newPatientcontact = document.getElementById("contactNoOfPatient").value;
+    var newPatientoccupation = document.getElementById("occupationOfPatient").value;
+    var newPatientmedicare = document.getElementById("medicareNoOfPatient").value;
+    var newPatienthealthcare = document.getElementById("healthCareNoOfPatient").value;
+    var newPatientemergname = document.getElementById("nameOfEmergencyContact").value;
+    var newPatientemergcontact = document.getElementById("contactNoOFEmergencyContact").value;
+    var gendervalue = '';
+    if (document.getElementById("m").checked){
+      gendervalue = document.getElementById("m").value;
+    } else {
+      gendervalue = document.getElementById("f").value;
+    }
+        parseCreatePatient(newPatientfname, newPatientlname, newPatientemail, newPatientaddress, newPatientDOB, newPatientcontact, newPatientoccupation, newPatientmedicare, newPatienthealthcare, newPatientemergname, newPatientemergcontact, gendervalue);
 }
 
-function parseCreatePatient(fname, lname) {
+function parseCreatePatient(fname, lname, email, address, dob, contact, occ, medi, health, emername, emerNo, gender) {
     var Patient = Parse.Object.extend("Patient");
     var patient = new Patient();
     patient.set("First_Name", fname);
     patient.set("Last_Name", lname);
+    patient.set("Address", address);
+    patient.set("Email", email);
+    patient.set("Gender", gender);
+    patient.set("Contact_No", contact);
+    patient.set("Emergency_Contact_Name", emername);
+    patient.set("Emergency_Contact_No", emerNo);
+    patient.set("Medicare_No", medi);
+    patient.set("DOB", dob);
+    patient.set("Occupation", occ);
+    patient.set("Health_Care_No", health);
       
     patient.save(null, {
       success: function(patient) {
@@ -19,7 +42,7 @@ function parseCreatePatient(fname, lname) {
       },
       error: function(patient, error) {
         // Show the error message somewhere and let the user try again.
-        alert("Error: " + error.code + " " + error.message);
+        //alert("Error: " + error.code + " " + error.message);
       }
     });
 }
