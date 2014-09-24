1ffe794f4a0fe6135f3c0c29bde2eacc8c75a806
Devesh Mitra <mitra.devesh@gmail.com>
2014-09-25 00:19:17 +1000

    IM is now working :)
    IM is now working :)

--- js/instantmessage.js
+++ js/instantmessage.js
@@ -2,10 +2,11 @@ var currentUser = Parse.User.current();
 
 imonload = function(){ 
     $("#im-link").addClass("active");
+    var pubnub;
     if (currentUser) {
         // YOU ARE LOGGED IN
-
         loadIM();
+        document.getElementById("post-im-button").onclick = postIM;
 
     } else {
         // YOU ARE NOT LOGGED IN
@@ -13,21 +14,23 @@ imonload = function(){
     }
     
     function loadIM() {        
-        var pubnub = PUBNUB.init({
+        pubnub = PUBNUB.init({
              publish_key: 'pub-c-80c26be5-7625-4f38-be95-7d7333e5b8ba',
              subscribe_key: 'sub-c-8878abb0-43e7-11e4-a249-02ee2ddab7fe'
          });
         
         pubnub.subscribe({
             channel: 'my_channel',
-            message: function(m){console.log(m)}
-         });
-        setTimeout(function()
-            {pubnub.publish({
+            message: function(m){$('#im-messages').append(m + '<br/>')}
+         });    
+    }
+    
+    function postIM() {
+        pubnub.publish({
                  channel: 'my_channel',        
-                 message: 'Hello from the PubNub Javascript SDK'
-             }); }
-        , 3000);     
+                 message: '' + currentUser.get('Staff_First_Name') + ' ' + currentUser.get('Staff_Last_Name') + ': ' + document.getElementById('new-message').value + ''
+             });
+        document.getElementById('new-message').value = '';
     }
 };
 
