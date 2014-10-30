f505ef1f3e87dca285b5dec5418cc7d3b089a3c4
mootz99 <muthu.udai99@gmail.com>
2014-10-31 00:32:06 +1100

    fixing generating pdf
    fixing generating pdf

--- js/patient.js
+++ js/patient.js
@@ -242,56 +242,10 @@ patientonload = function(){
 
         function generatePDF() {
         var doc = new jsPDF();
-
-        alert("Helo");        
+       
         var elementHandler = {
-            '#cssmenu': function (element, renderer) {
-            return true;
-            },
-            '#patient-name': function (element, renderer) {
-            return true;
-            },
-            '#patient-search-section': function (element, renderer) {
-            return true;
-            },
-            '#patient-admin-details': function (element, renderer) {
-            return true;
-            },
-            '#tablist': function (element, renderer) {
-            return true;
-            },
-            '#red': function (element, renderer) {
-            return true;
-            },
-            '#orange': function (element, renderer) {
-            return true;
-            },
             '#createPDF': function (element, renderer) {
-            return true;
-            },
-            '#patient-log': function (element, renderer) {
-            return true;
-            },
-            '#patientform': function (element, renderer) {
-            return true;
-            },
-            '#userform': function (element, renderer) {
-            return true;
-            },
-            '#appointmentform': function (element, renderer) {
-            return true;
-            },
-            '#home-link': function (element, renderer) {
-            return true;
-            },
-            '#user-dropdown': function (element, renderer) {
-            return true;
-            },
-            '#search-link': function (element, renderer) {
-            return true;
-            },
-            '#searchDate': function (element, renderer) {
-            return true;
+                    return true;
             }
         };
     var source = $('#yellow').html();
